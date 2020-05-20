40fea2e3de62d88ae5a2dbcdd7f647edd380fa43
harshr78 <harshrawal8295@gmail.com>
2020-05-20 17:34:42 +0530

    change in admin

--- register/admin.py
+++ register/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Student_Details
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
+# class registerClass(UserAdmin):
+#     list_display = ('student_admission_status','student_name')
+#     search_fields = ('student_name',)
+#     filter_horizontal=()
+#     list_filter=()
+#     fieldsets=()
 admin.site.register(Student_Details),
